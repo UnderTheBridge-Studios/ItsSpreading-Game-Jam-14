ba68e078db4f82c8c8b72f8344f56eee52c5ac23
Jaume Pla Ferriol <jaumeplaf@gmail.com>
2024-01-16 00:04:56 +0100

    Grow/Shrink effect implemented

--- Assets/Scripts/MaterialBlockGrowth.cs
+++ Assets/Scripts/MaterialBlockGrowth.cs
@@ -4,48 +4,81 @@ using UnityEngine;
 
 public class MaterialBlockGrowth : MonoBehaviour
 {
-        private MaterialPropertyBlock materialBlock;
-        private MeshRenderer meshRenderer;
-        //public float ticks = 5;
-        public float growthTime = 1;
-        public float initGrowth = 1;
-        public float goalGrowth = 0;
-        public bool DoGrow = false;
-        static float interpolation = 0.0f;
-        
+    //Material instance properties
+    private MaterialPropertyBlock materialBlock;
+    private MeshRenderer meshRenderer;
+    [SerializeField] private float currentPosition = 1;
+    [SerializeField] private float growthSpeed = 1;
 
-        void Start()
+    private bool isShrinking = false;
+    private bool isGrowing = false;
+    private float speedModifier = 1f;
+    private float interpolation = 0.0f;
+    private float initialPosition = 0;
+
+
+    void Start()
         {
+            print("E to grow, R to shrink");
             materialBlock = new MaterialPropertyBlock();
             meshRenderer = GetComponent<MeshRenderer>();
         }
-        void Update()
+
+    void Update()
     {
-        //breaks the game ATM :)
-            if((Input.GetKey("e") && !DoGrow)) {
-                    ChangePropertyBlock(Mathf.Lerp(initGrowth, goalGrowth, interpolation));
-                    interpolation += growthTime * Time.deltaTime;
-                    if(interpolation <= goalGrowth) 
-                    {
-                        float temp = goalGrowth;
-                        goalGrowth = initGrowth;
-                        interpolation = 0.0f;
-                        DoGrow = true;
-                    }
-            }
-        }
-        
-        private void ChangePropertyBlock(float amount)
+        if (Input.GetKeyDown("r") && currentPosition != 0 && !isShrinking)
         {
-            //for(int i = 0; i < ticks; i++) {
-            //amount = map(i, 0, ticks, 0, 1);
-            //print("Growth: " + amount);
+            //There is a delay between currentPosition and the function tick
+            initialPosition = currentPosition;
+            isShrinking = true;
+            isGrowing = false;
+        }
+        else if (Input.GetKeyDown("e") && currentPosition != 1 && !isGrowing)
+        {
+            //There is a delay between currentPosition and the function tick
+            initialPosition = currentPosition;
+            isGrowing = true;
+            isShrinking = false;
+        }
+
+        if (isShrinking)
+        {
+            Shrink();
+        }
+        else if (isGrowing)
+        {
+            Grow();
+        }
+    }
+
+    private void Shrink()
+    {
+        currentPosition = Mathf.Lerp(initialPosition, 0, interpolation);
+        ChangePropertyBlock(currentPosition);
+        interpolation += (growthSpeed*speedModifier) * Time.deltaTime;
+        if (currentPosition <= 0)
+        {
+            isShrinking = false;
+            interpolation = 0.0f;
+            //currentPosition = 0;
+        }
+    }
+
+    private void Grow()
+    {
+        currentPosition = Mathf.Lerp(initialPosition, 1, interpolation);
+        ChangePropertyBlock(currentPosition);
+        interpolation += (growthSpeed*speedModifier) * Time.deltaTime;
+        if (currentPosition >= 1)
+        {
+            isGrowing = false;
+            interpolation = 0.0f;
+            //currentPosition = 1;
+        }
+    } 
+    private void ChangePropertyBlock(float amount)
+        {
             materialBlock.SetFloat("_Growth", amount);
             meshRenderer.SetPropertyBlock(materialBlock);
-            }
-
-        float map(float s, float a1, float a2, float b1, float b2)
-        {
-            return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
         }
 }
