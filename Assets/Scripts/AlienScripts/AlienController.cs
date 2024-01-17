1ddb4f80cf167ddb34032d299576caae31a6b8b9
Hxampi <hxampi@gmail.com>
2024-01-17 14:05:54 +0100

    P11: Made transitins smoother

--- Assets/Scripts/AlienScripts/AlienController.cs
+++ Assets/Scripts/AlienScripts/AlienController.cs
@@ -5,37 +5,44 @@ using UnityEngine;
 public class AlienController : MonoBehaviour
 {
     [Range(0.0f, 1.0f)]
-    [SerializeField] private float m_size;
+    [SerializeField] private float m_growth;
+    [SerializeField] private float m_smoothness = 0.5f;
+    [SerializeField] private MeshRenderer m_meshRenderer;
+    [SerializeField] private GameObject m_visualCollider;
+
+    private float m_life;
     private MaterialPropertyBlock m_materialBlock;
-    private MeshRenderer m_meshRenderer;
     private BoxCollider m_collider;
 
     private void Awake()
     {
         m_materialBlock = new MaterialPropertyBlock();
-        m_meshRenderer = GetComponent<MeshRenderer>();
         m_collider = GetComponent<BoxCollider>();
-        SetSize(m_size);
+        m_life = m_growth;
+        SetSize(m_growth);
     }
 
     public void AlienGrowth (float value)
     {
-        m_size += value;
-        SetSize(m_size);
+        m_life += value;
+        m_growth = Mathf.SmoothStep(m_growth, m_life, m_smoothness);
+        SetSize(m_growth);
     }
 
     public void AlienDamage(float value)
     {
-        m_size -= value;
-        SetSize(m_size);
-
-        Debug.Log("Damaged");
+        m_life -= value;
+        m_growth = Mathf.SmoothStep(m_growth, m_life, m_smoothness);
+        SetSize(m_growth);
     }
 
     private void SetSize(float newSize)
     {
         if (newSize >= 0 && newSize <= 1)
         {
+            if (m_collider.enabled == false)
+                m_collider.enabled = true;
+
             ChangePropertyBlock(newSize);
             m_collider.size = new Vector3(newSize, 0.5f, newSize);
         }
@@ -44,6 +51,8 @@ public class AlienController : MonoBehaviour
             m_collider.enabled = false;
             Debug.Log("Alien death");
         }
+
+        m_visualCollider.transform.localScale = m_collider.size;
     }
     private void ChangePropertyBlock(float amount)
     {
