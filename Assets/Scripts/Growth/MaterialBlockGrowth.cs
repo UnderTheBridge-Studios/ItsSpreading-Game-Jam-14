3f1c06a29562a26a482e60466d43ad8b687f46da
pereruiz29 <65725598+pereruiz29@users.noreply.github.com>
2024-01-21 13:28:47 +0100

    AlienGroup

--- Assets/Scripts/Growth/MaterialBlockGrowth.cs
+++ Assets/Scripts/Growth/MaterialBlockGrowth.cs
@@ -16,10 +16,11 @@ public class MaterialBlockGrowth : MonoBehaviour
 
     private bool m_isChangingSize = false;
     private bool m_isGrowing = false;
-    private float m_speedModifier;
     private float m_interpolation = 0.0f;
     private float m_initialPosition = 0;
     private float m_targuetPosition;
+    private float m_speedModifier;
+    private float m_speedAwakeModifier;
 
     public float CurrentGrowth => m_currentPosition;
     public bool IsChangingSize => m_isChangingSize;
@@ -32,7 +33,6 @@ public class MaterialBlockGrowth : MonoBehaviour
         m_meshRenderer = GetComponent<MeshRenderer>();
     }
 
-
     void Update()
     {
         if (!m_isChangingSize)
@@ -41,15 +41,29 @@ public class MaterialBlockGrowth : MonoBehaviour
         ChangeSize();
     }
 
-    public void DoSizeChange(float targuetPosition, bool grow)
+    public IEnumerator Inicialize(float time, float growSpeed)
     {
-        m_initialPosition = m_currentPosition;
-        m_targuetPosition = targuetPosition;
-        m_speedModifier = grow ? m_speedGrowthModifier : m_speedShrinkModifier;
-        m_isGrowing = grow ? true : false;
-        m_isChangingSize = true;
+        m_speedAwakeModifier = growSpeed;
+        m_currentPosition = 0;
+        ChangePropertyBlock(m_currentPosition);
+        yield return new WaitForSeconds(time);
+        gameObject.SetActive(true);
+        DoSizeChange(1, true, true);
     }
 
+    public void DoSizeChange(float targuetPosition, bool grow, bool awake = false)
+    {
+        if (awake)
+            m_speedModifier = m_speedAwakeModifier;
+        else
+            m_speedModifier = grow ? m_speedGrowthModifier : m_speedShrinkModifier;
+
+        m_initialPosition = m_currentPosition;
+        m_targuetPosition = targuetPosition;
+        m_isGrowing = grow ? true : false; //non-optimized assignment
+        m_isChangingSize = true;
+    }
+    
     private void ChangeSize()
     {
         m_currentPosition = Mathf.Lerp(m_initialPosition, m_targuetPosition, m_interpolation);
