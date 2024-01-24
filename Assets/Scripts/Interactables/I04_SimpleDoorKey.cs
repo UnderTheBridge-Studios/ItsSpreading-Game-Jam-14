63a5d5d3e4bd86d30181ff81d51aeecd9fb07d38
Hxampi <hxampi@gmail.com>
2024-01-24 22:55:48 +0100

    P31: Added DoorKeys

---
+++ Assets/Scripts/Interactables/I04_SimpleDoorKey.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class I04_SimpleDoorKey : I08_SimpleDoor
+{
+    [SerializeField] private string m_keyID;
+
+    [Header("Failed interaction prompt")]
+    [SerializeField] private Sprite m_sprite;
+    [SerializeField] private string m_text;
+    [SerializeField] float m_time;
+
+    public override bool Interact(PlayerInteract interactor)
+    {
+        if (GameManager.instance.HasKey(m_keyID))
+        {
+            OpenDoor();
+            return true;
+        }
+        else
+        {
+            HUBManager.instance.UseActionPromp(m_sprite, m_text, m_time);
+            return false;
+        }
+    }
+}
