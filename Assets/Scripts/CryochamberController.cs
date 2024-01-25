1a3626470b0ca581fcc69e7c6d471dc8864a1cd7
Hxampi <hxampi@gmail.com>
2024-01-25 01:36:02 +0100

    P31: Added temporary animation for CryoChamber

--- Assets/Scripts/CryochamberController.cs
+++ Assets/Scripts/CryochamberController.cs
@@ -1,18 +1,23 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CryochamberController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Transform m_door;
+    [SerializeField] private float m_rotation = -60f;
+    [SerializeField] private float m_duration = 3f;
+    [SerializeField] private float m_timeBeforeOpen = 3f;
+
     void Start()
     {
-        
+        StartCoroutine(OpenDoor());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator OpenDoor()
     {
-        
+        yield return new WaitForSeconds(m_timeBeforeOpen);
+        m_door.DOLocalRotate(new Vector3(m_rotation, 0, 0), m_duration);
     }
 }
